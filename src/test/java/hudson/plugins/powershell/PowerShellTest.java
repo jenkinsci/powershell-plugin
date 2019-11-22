101bf775164f2e2a2dee7edd21a01cf6159ea1c1
Filipe Roque <froque@premium-minds.com>
2019-11-22 01:25:31 +0000

    upgrade parent pom to 3.53 added more unit tests added Jelly XSS prevention: https://wiki.jenkins.io/display/JENKINS/Jelly+and+XSS+prevention

--- src/test/java/hudson/plugins/powershell/PowerShellTest.java
+++ src/test/java/hudson/plugins/powershell/PowerShellTest.java
@@ -1,18 +1,85 @@
 package hudson.plugins.powershell;
 
-import org.jvnet.hudson.test.HudsonTestCase;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
+import hudson.model.Result;
+import hudson.model.queue.QueueTaskFuture;
+import org.junit.Assume;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  * @author Kohsuke Kawaguchi
  */
-public class PowerShellTest extends HudsonTestCase {
+public class PowerShellTest {
+
+    @Rule
+    public JenkinsRule r = new JenkinsRule();
+
+    @Test
     public void testConfigRoundtrip() throws Exception {
-        FreeStyleProject p = createFreeStyleProject();
+        FreeStyleProject p = r.createFreeStyleProject();
         PowerShell orig = new PowerShell("script", true);
         p.getBuildersList().add(orig);
-        submit(new WebClient().getPage(p,"configure").getFormByName("config"));
 
-        assertEqualBeans(orig, p.getBuildersList().get(PowerShell.class), "command");
+        JenkinsRule.WebClient webClient = r.createWebClient();
+        HtmlPage page = webClient.getPage(p, "configure");
+        HtmlForm form = page.getFormByName("config");
+        r.submit(form);
+
+        r.assertEqualBeans(orig, p.getBuildersList().get(PowerShell.class), "command");
     }
+
+    @Test
+    public void testBuildSuccess() throws Exception {
+        Assume.assumeTrue(isPowerShellAvailable());
+
+        FreeStyleProject project1 = r.createFreeStyleProject("project1");
+        project1.getBuildersList().add(new PowerShell("echo 'Hello World!'", true));
+
+        QueueTaskFuture<FreeStyleBuild> freeStyleBuildQueueTaskFuture = project1.scheduleBuild2(0);
+        FreeStyleBuild build = freeStyleBuildQueueTaskFuture.get();
+
+        r.assertBuildStatusSuccess(build);
+    }
+
+    @Test
+    public void testBuildBadCommandFails() throws Exception {
+        Assume.assumeTrue(isPowerShellAvailable());
+        FreeStyleProject project1 = r.createFreeStyleProject("project1");
+        project1.getBuildersList().add(new PowerShell("wrong command", true));
+
+        QueueTaskFuture<FreeStyleBuild> freeStyleBuildQueueTaskFuture = project1.scheduleBuild2(0);
+        FreeStyleBuild build = freeStyleBuildQueueTaskFuture.get();
+
+        r.assertBuildStatus(Result.FAILURE, build);
+    }
+
+    @Test
+    public void testBuildBadCommandsSucceeds() throws Exception {
+        Assume.assumeTrue(isPowerShellAvailable());
+        FreeStyleProject project1 = r.createFreeStyleProject("project1");
+        project1.getBuildersList().add(new PowerShell("wrong command", false));
+
+        QueueTaskFuture<FreeStyleBuild> freeStyleBuildQueueTaskFuture = project1.scheduleBuild2(0);
+        FreeStyleBuild build = freeStyleBuildQueueTaskFuture.get();
+
+        r.assertBuildStatus(Result.SUCCESS, build);
+    }
+
+    private boolean isPowerShellAvailable() {
+        return Stream.of(System.getenv("PATH").split(Pattern.quote(File.pathSeparator)))
+                .map(Paths::get)
+                .anyMatch(path -> Files.exists(path.resolve("pwsh")) || Files.exists(path.resolve("powershell")));
+    }
+
 }
